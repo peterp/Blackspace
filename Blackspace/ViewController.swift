948e243e24400e442f5962b13918116d8e67fdbb
Perceval FARAMAZ <perfaram@MacBook-Pro.local>
2017-07-15 10:25:43 +0200

    Don't use command line, make system calls instead ; can now add small spacers ; converted to Swift 3 syntax
    Signed-off-by: Perceval FARAMAZ <perfaram@MacBook-Pro.local>

--- Blackspace/ViewController.swift
+++ Blackspace/ViewController.swift
@@ -4,25 +4,48 @@
 //
 //  Created by Peter Pistorius on 12/10/15.
 //  Copyright © 2015 Peter Pistorius. All rights reserved.
-//  (I don't care.)
+//  Updated by Perceval Faramaz on 15/07/17.
+//  Portions copyright © 2017 Perceval Faramaz. All rights reserved.
+//  (Me neither.)
 //
 
 import Cocoa
 
 class ViewController: NSViewController {
-
-    func executeCommandLineTask(path: String, arguments: Array<String>) {
+    
+    func buildTileDictionary(_ small: Bool) -> Dictionary<String, String> {
+        var dict = Dictionary<String, String>()
+        dict["tile-type"] = (small) ? "small-spacer-tile" : "spacer-tile"
+        return dict
+    }
+    
+    func reloadDock() {
+        let runningApplications = NSWorkspace.shared().runningApplications
+        for app in runningApplications {
+            if app.bundleIdentifier == "com.apple.dock" {
+                kill( app.processIdentifier, SIGKILL );
+            }
+        }
+    }
+    
+    func insertSpacer(_ small: Bool) {
+        let dockDefaults = UserDefaults(suiteName: "com.apple.dock")
         
-        let task = NSTask()
-        task.launchPath = path
-        task.arguments = arguments
-        task.launch()
-        task.waitUntilExit()
+        guard var dockArray = dockDefaults?.array(forKey: "persistent-apps") else { return }
+        
+        dockArray.append( buildTileDictionary(small) )
+        
+        dockDefaults!.set(dockArray, forKey: "persistent-apps")
+        
+        reloadDock()
     }
 
-    @IBAction func didClickInsertVoid(sender: AnyObject) {
-        executeCommandLineTask("/usr/bin/defaults", arguments: ["write", "com.apple.dock", "persistent-apps", "-array-add", "{\"tile-type\"=\"spacer-tile\";}"]);
-        executeCommandLineTask("/usr/bin/killall", arguments: ["Dock"]);
+    @IBAction func didClickInsertVoid(_ sender: AnyObject) {
+        insertSpacer(false)
+    }
+    
+    @IBAction func didClickInsertSmallVoid(_ sender: AnyObject) {
+        insertSpacer(true)
     }
 }
 
